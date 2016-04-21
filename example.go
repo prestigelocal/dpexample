fe7be12b33d4ab1ee71a4d4d2e45fd3fc450026c
vsukhin <vsukhin@virtuososoft.com>
2016-04-21 18:18:33 +0300

    Initial commit

---
+++ example.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/deferpanic/deferclient/deferstats"
+	"net/http"
+	"time"
+)
+
+// fast test
+func fastHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "this request is fast")
+}
+
+// slow test
+func slowHandler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(6 * time.Second)
+	fmt.Fprintf(w, "this request is slow")
+}
+
+// panic test
+func panicHandler(w http.ResponseWriter, r *http.Request) {
+	panic("There is no need for panic")
+	fmt.Fprintf(w, "this request is panic")
+}
+
+func main() {
+	dps := deferstats.NewClient("z57z3xsEfpqxpr0dSte0auTBItWBYa1c")
+
+	go dps.CaptureStats()
+
+	// no need to change these?
+	http.HandleFunc("/fast", dps.HTTPHandlerFunc(fastHandler))
+	http.HandleFunc("/slow", dps.HTTPHandlerFunc(slowHandler))
+	http.HandleFunc("/panic", dps.HTTPHandlerFunc(panicHandler))
+
+	http.ListenAndServe(":3000", nil)
+}
